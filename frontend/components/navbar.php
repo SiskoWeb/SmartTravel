dc4c7792c4dcea77ca63b15151bc579a93534c00
siskoweb <sisko4dev@gmail.com>
2023-12-21 19:35:29 +0100

    chnage logo

--- frontend/components/navbar.php
+++ frontend/components/navbar.php
@@ -28,10 +28,10 @@
 </nav>
 
 
-<nav class="bg-white border-gray-200 py-3 dark:bg-gray-900 shadow-b shadow-lg">
+<nav class="bg-white border-gray-200 py-2 dark:bg-gray-900 shadow-b shadow-lg">
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
         <a href="#" class="flex items-center">
-            <img src="assets/logo.png" class="h-6 mr-3 sm:h-9" alt="travel Logo">
+            <img src="assets/logo.png" class="h-8 mr-3 sm:h-12" alt="travel Logo">
         </a>
         <div class="flex items-center lg:order-2">
             <div class="hidden mt-2 mr-4 sm:inline-block">
