629f51bad52e00b717cd80c80e4416d6d29a47ee
siskoweb <sisko4dev@gmail.com>
2023-12-21 17:19:13 +0100

    finish research page logic

--- frontend/Research.php
+++ frontend/Research.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/ea3542be0c.js" crossorigin="anonymous"></script>
+
+    <title>SmartTravel</title>
+    <style>
+        /* Add styling for the bus_img */
+        #bus_img {
+            transition: right 20s ease-in-out;
+        }
+    </style>
 </head>
-<body>
+<?php require('components/navbar.php')?>
+<body >
+<section class="bg-cover bg-center relative  w-full h-[300px] flex items-end justify-center" style="background-image: url(assets/heroResearch.png)">
+       <div class="bg-white rounded-tr-md rounded-tl-md w-3/4 h-3/4">
+       <?php require('components/form.php')?>
+       </div>
+    </section>
+
+
 <script src="scripts/Research.js"></script>
 </body>
 </html>
--- frontend/scripts/Research.js
+++ frontend/scripts/Research.js
@@ -1,9 +1,34 @@
 document.addEventListener('DOMContentLoaded', async function () {
 
-    //after page loaded get all querys and fetch data with i
+
+
+    const tripsData = [];
+
+
+
+
+
+    // Fetch cities and populate the dropdowns
+    try {
+        const departure = document.getElementById('departure');
+        const destination = document.getElementById('destination');
+        const citiesPromise = await fetch('cities.json');
+        const data = await citiesPromise.json();
+
+        data.forEach(city => {
+            // loop through list cities and add it as option
+            addOptionToSelect(departure, city.city);
+            addOptionToSelect(destination, city.city);
+        });
+    } catch (error) {
+        console.error("Error fetching cities:", error);
+    }
+
+
+
+
+    // Get query parameters from the URLl
     const urlParams = new URLSearchParams(window.location.search);
-
-
     const dateParam = urlParams.get('date');
     const departureParam = urlParams.get('departure');
     const destinationParam = urlParams.get('destination');
@@ -11,21 +36,123 @@ document.addEventListener('DOMContentLoaded', async function () {
     const maxPriceParam = urlParams.get('maxPrice') ?? null;
     const orderParam = urlParams.get('order') ?? null;
 
-    const url = `http://localhost/travel/backend/trip.php?action=filter&date=${dateParam}&departure=${departureParam}&destination=${destinationParam}&minPrice=${minPriceParam}&maxPrice=${maxPriceParam}&order=${orderParam}`;
+    // Set default values in form inputs
+    document.getElementById('departure').value = departureParam || '';
+    document.getElementById('destination').value = destinationParam || '';
+    document.getElementById('date').value = dateParam || '';
 
-    fetch(url)
-        .then(response => {
+
+
+
+
+
+    // function to build the URL based on the form inputs
+    function buildUrl() {
+        const queryParams = new URLSearchParams({
+            date: document.getElementById('date').value,
+            departure: departure.value,
+            destination: destination.value,
+            // minPrice: minPriceParam,
+            // maxPrice: maxPriceParam,
+            // order: orderParam
+        });
+        return `http://localhost/travel/backend/trip.php?action=filter&${queryParams.toString()}`;
+    }
+
+
+
+
+    // Fetch data when the page loads
+    try {
+        const url = buildUrl();
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+        }
+        const data = await response.json();
+        console.log("Initial fetch successful. Data:", data);
+        // Handle the data as needed
+    } catch (error) {
+        console.error("Initial fetch failed:", error);
+        // Handle errors
+    }
+
+
+
+
+    // Event listener for "Find Trip"  brn
+    const findTripBtn = document.getElementById('findTripBtn')
+    findTripBtn.addEventListener('click', onSubmit);
+
+    async function onSubmit() {
+
+        const departureValue = departure.value.trim();
+        const destinationValue = destination.value.trim();
+        const date = document.getElementById('date').value.trim();
+
+        const error_msg = document.getElementById('error_msg');
+
+        // send inputs to validat it
+        if (!validateInputs(departureValue, destinationValue, date, error_msg)) {
+            return;
+        }
+
+        const queryParams = new URLSearchParams({
+            date: date,
+            departure: departureValue,
+            destination: destinationValue
+        });
+
+
+        // Update the URL without reloading the page
+        history.pushState({}, '', '?' + queryParams.toString());
+
+
+
+        // Fetch data with the updated URL 
+        // buildUrl create url from query everytime
+        try {
+            const url = buildUrl();
+            const response = await fetch(url);
             if (!response.ok) {
                 throw new Error(`Network response was not ok: ${response.statusText}`);
             }
-            return response.json(); // or response.text() or response.blob() based on the expected response type
-        })
-        .then(data => {
+            const data = await response.json();
             console.log("Fetch successful. Data:", data);
             // Handle the data as needed
-        })
-        .catch(error => {
+        } catch (error) {
             console.error("Fetch failed:", error);
             // Handle errors
-        });
-})
+        }
+    }
+
+
+
+
+    // function to add an option <citeis> to a select element
+    function addOptionToSelect(selectElement, optionValue) {
+        const option = document.createElement('option');
+        option.value = optionValue;
+        option.text = optionValue;
+        selectElement.appendChild(option);
+    }
+
+
+
+    // function for input validation
+    function validateInputs(departure, destination, date, error_msg) {
+        if (destination === "") {
+            error_msg.textContent = "Please Select destination";
+            return false;
+        }
+        if (departure === "") {
+            error_msg.textContent = "Please Select departure.";
+            return false;
+        }
+        if (date === "") {
+            error_msg.textContent = "Please Select date.";
+            return false;
+        }
+        return true;
+    }
+});
